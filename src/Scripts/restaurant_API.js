2c9498043664b8d54b2692fdcc4d35bcfa700e4c
scottsilver <scottsilver47@gmail.com>
2019-06-12 10:11:20 -0500

    changed fetch call to make it searchable to user

--- src/Scripts/restaurant_API.js
+++ src/Scripts/restaurant_API.js
@@ -1,5 +1,15 @@
 
-function dude() {
-    return fetch(`https://developers.zomato.com/api/v2.1/location_details?entity_id=1138&entity_type=city&apikey=${zomato_keys}`)
+// function zomatoFetch () {
+//     return fetch(`https://developers.zomato.com/api/v2.1/location_details?entity_id=1138&entity_type=city&apikey=${zomato_keys}`)
+//     .then( foodFromAPI => foodFromAPI.json())
+// }
+function zomatoFetch (userInput) { 
+    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&&entity_type=city&q=${userInput}&sort=rating&apikey=${api_keys.zomato_keys}`)
     .then( foodFromAPI => foodFromAPI.json())
-}
+    .then ( restaurants => {
+    console.log(restaurants)
+    let results = document.querySelector("#search-results")
+  })
+  }
+  zomatoFetch("burger")
+  
