8394e40649aa62af158b93de31605adb1061ac95
Karla Gallegos <kgalleg618@gmail.com>
2019-06-12 15:39:29 -0500

    created a loop for my API to print on the DOM

--- src/Scripts/parks_API.js
+++ src/Scripts/parks_API.js
@@ -1,14 +1,25 @@
-// if you are working with the Parks API and are having a hard time accessing the address for the park, try changing `74d7-b74t.json` to `xbru-cfzi.json` in your fetch request
+// if you are working with the Parks API and are having a hard time accessing the address for the park, try changing `74d7-b74t.json` to `.json` in your fetch request
 
 
 function parkfind(parksearch) {
-    fetch(`https://data.nashville.gov/resource/74d7-b74t.json?${parksearch}=Yes&$$app_token=${park_app_keys.APP_TOKEN}`)
+    fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?${parksearch}=Yes&$$app_token=${park_app_keys.APP_TOKEN}`)
     .then(parkData => parkData.json())
     .then( parks => {
-        // console.log(parks)
+        console.log(parks)
     parks.forEach(park => {
         console.log(park)
         })
+        parkNames.innerHTML = ""
+        for (let i = 0; i < 5; i++) {
+        //   console.log(parks)
+        parkNames.innerHTML += 
+        `
+        <div id=${i}>
+        <p>${parks[i].park_name}: ${parks[i].mapped_location_address}, ${parks[i].mapped_location_city}, ${parks[i].mapped_location_state}</p>
+        <button id="save_park-${i}">Save</button>
+        </div>
+        `
+        }
     })
 }
     
@@ -16,6 +27,9 @@ document.querySelector("#place-search").addEventListener("click", function(){
     const parkSearchValues = document.querySelector("#parkSearch").value
     parkfind(parkSearchValues)  
 })
+
+const parkNames = document.querySelector("#search-results")
+    
 
 
 
