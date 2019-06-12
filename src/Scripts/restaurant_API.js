f30d319096abf031ab0d2a854aa9f25fc84536a9
scottsilver <scottsilver47@gmail.com>
2019-06-12 11:39:29 -0500

    nothing new

--- src/Scripts/restaurant_API.js
+++ src/Scripts/restaurant_API.js
@@ -1,15 +1,15 @@
 
-// function zomatoFetch () {
-//     return fetch(`https://developers.zomato.com/api/v2.1/location_details?entity_id=1138&entity_type=city&apikey=${zomato_keys}`)
-//     .then( foodFromAPI => foodFromAPI.json())
-// }
+
 function zomatoFetch (userInput) { 
-    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&&entity_type=city&q=${userInput}&sort=rating&apikey=${api_keys.zomato_keys}`)
+    fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&&entity_type=city&q=${userInput}&sort=rating&start=0&count=20&apikey=${api_keys.zomato_keys}`)
     .then( foodFromAPI => foodFromAPI.json())
     .then ( restaurants => {
     console.log(restaurants)
     let results = document.querySelector("#search-results")
   })
   }
-  zomatoFetch("burger")
+
+let searchButton = document.getElementById("#search-results");
+console.log('searchButton', searchButton)
   
+
