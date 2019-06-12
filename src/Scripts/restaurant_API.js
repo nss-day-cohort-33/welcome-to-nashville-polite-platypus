207fb9e051253616699cfe7e5ee77b8338beea7b
scottsilver <scottsilver47@gmail.com>
2019-06-12 14:38:32 -0500

    added event listener

--- src/Scripts/restaurant_API.js
+++ src/Scripts/restaurant_API.js
@@ -1,16 +1,25 @@
-// let searchButton = document.getElementById("#search-results");
-// searchButton.addEventListener("click", function( typedSearch))
-// console.log('searchButton', searchButton)
+let searchButton = document.getElementById("restaurant-search");
+searchButton.addEventListener("click", function() {
+    let searchTerm = document.getElementById("cuisine-input").value
+    zomatoFetch(searchTerm)
+    console.log('searchButton', searchButton)
+})
 
 function zomatoFetch (userInput) { 
     fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&&entity_type=city&q=${userInput}&sort=rating&start=0&count=20&apikey=${api_keys.zomato_keys}`)
     .then( foodFromAPI => foodFromAPI.json())
-    .then ( restaurants => {
-    console.log(restaurants)
-    let results = document.querySelector("#search-results")
-})
+    .then ( results => {
+    console.log(results)
+    let foodResults = document.querySelector("#search-results")
+        foodResults.innerHTML = ""
+    for (let i = 0; i < 5; i++) {
+        console.log(results)
+        foodResults.innerHTML += 
+        `<p>${results.restaurants[i].restaurant.name}><button id="save_parts">Save</button> </p>`
+        }
+    })
 }
-zomatoFetch("burgers")
+
 
 // const restaurant=[]
 // const restaurantSearch = restaurants.filter (function ()) {
@@ -26,9 +35,4 @@ zomatoFetch("burgers")
 //         let newObj = obj;
 //         createRestaurantOption(newObj)
 //     }
-// for (let i = 0; i < 5; i++) {
-//     console.log(restaurants)
-//     results.innerHTML += 
-//     `<p>${results.restaurants[0].restaurant.name}><button id="save_parts">Save</button> </p>`
-    
-//     });
+
