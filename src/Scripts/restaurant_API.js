b4fc63c01819b94feece0f3cf5cc1c2cbddb79e5
scottsilver <scottsilver47@gmail.com>
2019-06-12 21:14:23 -0500

    added event listener for save button

--- src/Scripts/restaurant_API.js
+++ src/Scripts/restaurant_API.js
@@ -5,26 +5,53 @@ searchButton.addEventListener("click", function() {
     console.log('searchButton', searchButton)
 })
 
+
+
+
+let saveButton = document.getElementById("search-results");
+saveButton.addEventListener("click", function(){
+    console.log(event.target)
+   
+    if (event.target.classList.contains("saveButton")) {
+    console.log(event.target.id.slice(-1))
+    let saveItem = searchResults[+event.target.id.slice(-1)].restaurant.name
+    let itineraryDOM = document.getElementById("restaurant-itinerary")
+    itineraryDOM.innerHTML = ""
+    itineraryDOM.innerHTML += "Restaurant:   " + saveItem
+    console.log(saveItem) 
+}   
+})
+
+let searchResults = []
+
+
 function zomatoFetch (userInput) { 
     fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&&entity_type=city&q=${userInput}&sort=rating&start=0&count=20&apikey=${api_keys.zomato_keys}`)
     .then( foodFromAPI => foodFromAPI.json())
     .then ( results => {
     console.log(results)
+    searchResults = results.restaurants
     let foodResults = document.querySelector("#search-results")
         foodResults.innerHTML = ""
     for (let i = 0; i < 5; i++) {
         console.log(results)
         foodResults.innerHTML += 
-        `<p>${results.restaurants[i].restaurant.name}><button id="save_parts">Save</button> </p>`
+        `<div id=results${i}>
+        <p>${results.restaurants[i].restaurant.name}</p>
+        <button class = "saveButton" id="save_parts-${i}">Save</button>
+        </div> `
         }
     })
 }
+// for (let i = 0; i < 5; i++) {
+//     console.log(events)
+//   results.innerHTML +=
+// 
 
 
 // const restaurant=[]
 // const restaurantSearch = restaurants.filter (function ()) {
-    
-// }
+ 
 
 
 
