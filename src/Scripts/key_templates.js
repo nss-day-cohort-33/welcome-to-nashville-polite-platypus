e6f584a71212d5ea323e0bb27da184afb1d85a46
scottsilver <scottsilver47@gmail.com>
2019-06-14 09:21:24 -0500

    changed variable for results click event

--- src/Scripts/key_templates.js
+++ src/Scripts/key_templates.js
@@ -2,6 +2,5 @@ const api_keys = {
  eventbrite_token: "Your Eventbrite token goes here",
 park_APP_TOKEN: "Your Park App token goes here",
 zomato_keys: "your Zomato key goes here",
-ticket_master_key: "Your Ticket Master key goes here"
-  
+ticket_master_key: "Your Ticket Master key goes here" 
 }
